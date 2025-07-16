7d00deffa56d56befc60d94b5e458cc18f2423f1
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-16 12:50:10 +0530

    Time: 1 ms (67.57%), Space: 59.1 MB (27.03%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.ts
@@ -0,0 +1,12 @@
+function stableMountains(height: number[], threshold: number): number[] {
+    const result = [];
+
+    for (let i = 1; i < height.length; i++) {
+        if (height[i - 1] > threshold) {
+            result.push(i);
+        }
+    }
+
+    return result;
+
+};
