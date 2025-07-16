c8bc43b772dea82b6df0b445d469a4743c4ecf1a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-16 19:13:14 +0530

    Time: 0 ms (100%), Space: 58.3 MB (7.27%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.ts
@@ -0,0 +1,12 @@
+function isArraySpecial(nums: number[]): boolean {
+    const n = nums.length;
+    if(n == 1) return true;
+
+    for(let i = 1; i < n; i++) {
+        if((nums[i] % 2 == 1 && nums[i-1] % 2 == 1) || (nums[i] % 2 == 0 && nums[i-1] % 2 == 0)) {
+            return false;
+        }
+    }
+
+    return true
+};
