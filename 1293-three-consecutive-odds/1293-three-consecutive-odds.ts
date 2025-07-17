a50f12c2a43ec474cbfdbc773726a37c6aa3d746
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-17 23:32:13 +0530

    Time: 2 ms (13.83%), Space: 56.3 MB (13.83%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.ts
@@ -0,0 +1,10 @@
+function threeConsecutiveOdds(arr: number[]): boolean {
+    const n = arr.length;
+    for(let i = 2; i < n; i++) {
+        if(arr[i] % 2 === 1 && arr[i-1] % 2 === 1 && arr[i-2] % 2 === 1) {
+            return true;
+        }
+    }
+
+    return false;
+};
