aa3361094a0f243ef2ebb391b0161a3db44d7ff2
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-26 22:53:08 +0530

    Time: 1 ms (42.61%), Space: 57 MB (22.72%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.ts
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.ts
@@ -1,11 +1,9 @@
 function runningSum(nums: number[]): number[] {
     const n = nums.length;
-    const ans = new Array(n).fill(0);
-    ans[0] = nums[0];
 
     for(let i = 1; i < n; i++) {
-        ans[i] = nums[i] + ans[i-1];
+        nums[i] = nums[i] + nums[i-1];
     }
 
-    return ans;
+    return nums;
 };
