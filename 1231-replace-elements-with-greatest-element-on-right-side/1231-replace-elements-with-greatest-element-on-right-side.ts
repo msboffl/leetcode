440964198d12ef3e4a8a911a79c183cf8a3ba5ec
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-26 08:48:16 +0530

    Time: 1 ms (86.11%), Space: 65.2 MB (42.36%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.ts
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.ts
@@ -1,14 +1,14 @@
 function replaceElements(arr: number[]): number[] {
     const n = arr.length;
-    const ans: number[] = [];
     let maxRight = -1;
 
     for(let i = n-1; i >= 0; i--) {
-        ans.push(maxRight);
-        if(arr[i] > maxRight) {
-            maxRight = arr[i];
+        const current = arr[i];
+        arr[i] = maxRight;
+        if(current > maxRight) {
+            maxRight = current;
         }
     }
 
-    return ans.reverse();
+    return arr;
 };
