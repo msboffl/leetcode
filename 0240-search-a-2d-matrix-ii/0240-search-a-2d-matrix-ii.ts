015ed334b47e2fb145842a362de7d216538e5647
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-28 00:44:13 +0530

    Time: 53 ms (31.88%), Space: 58.6 MB (55.8%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.ts
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.ts
@@ -1,15 +1,14 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
     const rows = matrix.length;
-    for(let i = 0; i < rows; i++) {
-        let left = 0;
-        let right = matrix[i].length;
+    const cols = matrix[0].length;
 
-        while(left <= right) {
-            let mid = Math.floor((left + right)/2);
-            if(matrix[i][mid] === target) return true;
-            else if(matrix[i][mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
+    let row = 0;
+    let col = cols - 1;
+
+    while(row < rows && col >= 0) {
+        if(matrix[row][col] === target) return true;
+        else if(matrix[row][col] < target) row++;
+        else col--;
     }
 
     return false
