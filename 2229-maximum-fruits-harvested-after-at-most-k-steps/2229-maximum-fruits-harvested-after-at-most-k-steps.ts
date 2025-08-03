b1a976a9a98a0c7e3f5c8f9ed48ae60045a65730
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-03 09:46:59 +0530

    Time: 198 ms (80%), Space: 87 MB (80%) - LeetHub

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.ts
@@ -0,0 +1,56 @@
+var maxTotalFruits = function (fruits, startPos, k) {
+    const n = fruits.length;
+    const sum = new Array(n + 1).fill(0);
+    const indices = new Array(n).fill(0);
+    sum[0] = 0;
+    for (let i = 0; i < n; i++) {
+        sum[i + 1] = sum[i] + fruits[i][1];
+        indices[i] = fruits[i][0];
+    }
+    let ans = 0;
+    for (let x = 0; x <= Math.floor(k / 2); x++) {
+        /* move left x steps, then move right k - x steps. */
+        let y = k - 2 * x;
+        let left = startPos - x;
+        let right = startPos + y;
+        let start = lowerBound(indices, 0, n - 1, left);
+        let end = upperBound(indices, 0, n - 1, right);
+        ans = Math.max(ans, sum[end] - sum[start]);
+        /* move right x steps, then move left k - x steps. */
+        y = k - 2 * x;
+        left = startPos - y;
+        right = startPos + x;
+        start = lowerBound(indices, 0, n - 1, left);
+        end = upperBound(indices, 0, n - 1, right);
+        ans = Math.max(ans, sum[end] - sum[start]);
+    }
+    return ans;
+};
+
+const lowerBound = (arr, left, right, val) => {
+    let res = right + 1;
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] >= val) {
+            res = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return res;
+};
+
+const upperBound = (arr, left, right, val) => {
+    let res = right + 1;
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (arr[mid] > val) {
+            res = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return res;
+};
