a82132354e75a8bc3c18f812559a60a75a6ac1e8
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-04 14:15:47 +0530

    Time: 30 ms (77.08%), Space: 67.1 MB (84.03%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.ts
@@ -0,0 +1,28 @@
+function totalFruit(fruits: number[]): number {
+    const fruitsLen: number = fruits.length;
+    const backets: Map<number, number> = new Map();
+
+    let result: number = 0;
+
+    for (let left = 0, right = 0; right < fruitsLen; ++right) {
+        const rightFruit: number = fruits[right];
+
+        backets.set(rightFruit, (backets.get(rightFruit) ?? 0) + 1);
+
+        while (backets.size > 2) {
+            const leftFruit: number = fruits[left];
+
+            backets.set(leftFruit, backets.get(leftFruit)! - 1);
+
+            if (backets.get(leftFruit) === 0) {
+                backets.delete(leftFruit);
+            }
+
+            ++left;
+        }
+
+        result = Math.max(result, right - left + 1);
+    }
+
+    return result;
+}
