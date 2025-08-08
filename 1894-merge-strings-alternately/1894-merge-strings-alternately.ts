7780d7ab74fad136e895483fabd165f27a6219cb
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-08 20:55:35 +0530

    Time: 50 ms (30.33%), Space: 56.3 MB (30.08%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -1,18 +1,8 @@
 function mergeAlternately(word1: string, word2: string): string {
     let ans = "";
-    let i = 0;
-    let j = 0;
-
-    while(i < word1.length && j < word2.length) {
-        ans += word1[i++];
-        ans += word2[j++];
-    }
-    while(i < word1.length) {
-        ans += word1[i++];
-    }
-
-    while(j < word2.length) {
-        ans += word2[j++];
+    for(let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if(i < word1.length) ans += word1[i];
+        if(i < word2.length) ans += word2[i];
     }
 
     return ans;
