966c2a7a73cc521f62b9c0e7ebc68158d65988d2
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-09 10:33:23 +0530

    Time: 1 ms (45.24%), Space: 57.7 MB (55.29%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.ts
+++ 0231-power-of-two/0231-power-of-two.ts
@@ -1,8 +1,3 @@
 function isPowerOfTwo(n: number): boolean {
-    if(n <= 0) return false;
-    for(let i = 0; 2**i <= n; i++) {
-        if(2**i == n) return true;
-    }
-
-    return false;
+    return n > 0 && (n & (n-1)) === 0;
 };
