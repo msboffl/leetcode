01810ec28226ab7015fe57dbf9de93fcc6c83a6c
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-10 12:36:52 +0530

    Time: 0 ms (100%), Space: 55.7 MB (38.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.ts
+++ 0050-powx-n/0050-powx-n.ts
@@ -1,3 +1,13 @@
 function myPow(x: number, n: number): number {
-    return Math.pow(x, n);
+    const negative = n < 0;
+
+    let total = 1;
+    n = Math.abs(n);
+    while(n > 0) {
+        if(n % 2 === 1) total *= x;
+        x *= x;
+        n = Math.floor(n/2);
+    }
+
+    return negative ? 1/total : total;
 };
