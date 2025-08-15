646718245d0eec76a0a7018a7b0f0485b628cb0a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-15 13:39:44 +0530

    Time: 0 ms (100%), Space: 56.5 MB (93.75%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.ts
+++ 0342-power-of-four/0342-power-of-four.ts
@@ -1,9 +1,7 @@
 function isPowerOfFour(n: number): boolean {
-    if(n <= 0) return false;
-    
-    while(n % 4 === 0) {
-        n /= 4
-    } 
-
+    if (n < 1) return false;
+    while (n % 4 === 0) {
+        n /= 4;
+    }
     return n === 1;
 };
