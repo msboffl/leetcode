879899c2981c376687bcd35f358ea4350eec61fe
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-18 10:51:14 +0530

    Time: 50 ms (69.67%), Space: 58.3 MB (6.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -1,15 +1,14 @@
 function reverse(x: number): number {
-    const isNeg = x < 0;
-    x = Math.abs(x);
+    const neg = x < 0;
+
+    let num = Math.abs(x);
     let rev = 0
-    const INT_MAX = 2 ** 31 - 1;
-    while (x > 0) {
-        let rem = x % 10;
-        
-        rev = (rev * 10) + rem;
-        x = Math.floor(x / 10);
+    while(num > 0) {
+        let ld = num % 10;
+        rev = ld + (rev * 10);
+        num = Math.floor(num / 10);
     }
 
-    if(rev > INT_MAX) return 0;
-    return isNeg ? -rev: rev;
+    if(rev > 2**31 - 1) return 0;
+    return neg ? -rev: rev;
 };
