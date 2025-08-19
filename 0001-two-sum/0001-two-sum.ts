8871d2b5e4795b77f0d084ed9e4c7def25fe5605
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-19 08:36:39 +0530

    Time: 2 ms (75.53%), Space: 57.9 MB (18.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,11 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
+    const map = new Map();
     for(let i = 0; i < nums.length; i++) {
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        let comp = target - nums[i];
+        if(map.has(comp)) {
+            return [i, map.get(comp)]
+        } else {
+            map.set(nums[i], i)
         }
     }
-
-    return [];
 };
