eeeb49f69fe260fb4d555b4eed7f8d0bcf9060ba
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-24 12:28:21 +0530

    Time: 2 ms (58.28%), Space: 61 MB (78.15%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.ts
@@ -0,0 +1,16 @@
+function longestSubarray(nums: number[]): number {
+    let left = 0, zeros = 0, res = 0;
+    
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) zeros++;
+        
+        while (zeros > 1) {
+            if (nums[left] === 0) zeros--;
+            left++;
+        }
+        
+        res = Math.max(res, right - left);
+    }
+    
+    return res;
+}
