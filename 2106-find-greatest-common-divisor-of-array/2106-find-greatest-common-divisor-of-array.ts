06ba3a3729e5ac4c86df53471d7bf6e80e7aeef5
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-28 12:06:15 +0530

    Time: 2 ms (22.22%), Space: 55.6 MB (61.11%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.ts
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.ts
@@ -1,10 +1,21 @@
-function findGCD(nums: number[]): number {
-    let a = Math.min(...nums);
-    let b = Math.max(...nums);
-
-    while(b != 0) {
-        [a, b] = [b, a % b];
+function gcd(a, b) {
+    while(b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
     }
 
     return a;
+}
+
+function findGCD(nums: number[]): number {
+    let max = -Infinity;
+    let min = +Infinity;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > max) max = nums[i];
+        if(nums[i] < min) min = nums[i]
+    }
+
+    return gcd(min, max);
 };
