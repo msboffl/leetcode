967a635579ad3aa3943cca16aef0b8e7094d2f68
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-06 09:17:24 +0530

    Time: 2 ms (34.65%), Space: 59.5 MB (62.47%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.ts
+++ 2058-concatenation-of-array/2058-concatenation-of-array.ts
@@ -1,11 +1,3 @@
 function getConcatenation(nums: number[]): number[] {
-    const n = nums.length;
-    const ans = []
-
-    for(let i = 0; i < n; i++) {
-        ans[i] = nums[i];
-        ans[i + n] = nums[i]
-    }
-
-    return ans;
+    return [...nums, ...nums];
 };
