c42a28217f1580943b1b684430ab3f542e48027d
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-07 10:22:43 +0530

    Time: 2 ms (80.33%), Space: 56.5 MB (100%) - LeetHub

--- 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.ts
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.ts
@@ -1,19 +1,14 @@
 function leftRightDifference(nums: number[]): number[] {
     const n = nums.length;
-    const ans = [];
+    const ans = new Array(n).fill(0);
+
+    const totalSum = nums.reduce((acc, val) => acc + val, 0);
+    let leftSum = 0
 
     for(let i = 0; i < n; i++) {
-        let leftSum = 0
-        for(let j = 0; j < i; j++) {
-            leftSum += nums[j]
-        }
-
-        let rightSum = 0
-        for(let j = i+1; j < n; j++) {
-            rightSum += nums[j]
-        }
-
-        ans.push(Math.abs(leftSum - rightSum))
+        let rightSum = totalSum - leftSum - nums[i];
+        ans[i] = Math.abs(leftSum - rightSum);
+        leftSum += nums[i];
     }
 
     return ans;
