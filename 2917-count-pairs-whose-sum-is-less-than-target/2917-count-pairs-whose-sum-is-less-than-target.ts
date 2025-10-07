7479d3057397d8e2702ae6446e5b97635e0f9546
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-07 10:45:20 +0530

    Time: 2 ms (46.03%), Space: 56 MB (95.24%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.ts
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.ts
@@ -2,9 +2,17 @@ function countPairs(nums: number[], target: number): number {
     const n = nums.length;
     let count = 0;
 
-    for(let i = 0; i < n; i++) {
-        for(let j = i+1; j < n; j++) {
-            if(nums[i] + nums[j] < target) count++;
+    nums.sort((a, b) => a - b);
+    
+    let left = 0;
+    let right = n-1;
+
+    while(left < right) {
+        if(nums[left] + nums[right] < target) {
+            count += right - left;
+            left++;
+        } else {
+            right--;
         }
     }
 
