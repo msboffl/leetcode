985bb400bb054158d52ef88f00fdf9f9c44d7de9
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-07 10:48:43 +0530

    Time: 0 ms (100%), Space: 57.3 MB (80.39%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.ts
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.ts
@@ -1,9 +1,9 @@
 function numberOfEmployeesWhoMetTarget(hours: number[], target: number): number {
+    const n = hours.length;
     let count = 0;
-    for(let i = 0; i < hours.length; i++) {
-        if(hours[i] >= target) {
-            count++;
-        }
+
+    for(let i = 0; i < n; i++) {
+        if(hours[i] >= target) count++
     }
 
     return count;
