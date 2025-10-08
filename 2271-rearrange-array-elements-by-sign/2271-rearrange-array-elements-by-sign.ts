06eb4dab0df198b37bcb680ccfb0b025d4fe41cc
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-08 18:26:12 +0530

    Time: 23 ms (39.82%), Space: 100.8 MB (26.55%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.ts
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.ts
@@ -1,18 +1,16 @@
 function rearrangeArray(nums: number[]): number[] {
-    const positives = [], negatives = [];
+    const n = nums.length;
     const ans = []
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] > 0) positives.push(nums[i]);
-        else negatives.push(nums[i]);
-    }
-    
-    let i = 0, j = 0;
 
-    while(i < positives.length && negatives.length) {
-        ans.push(positives[i]);
-        ans.push(negatives[j]);
-        i++;
-        j++;
+    let posIndex = 0, negIndex = 1;
+    for(let i = 0; i < n; i++) {
+        if(nums[i] > 0) {
+            ans[posIndex] = nums[i];
+            posIndex += 2
+        } else {
+            ans[negIndex] = nums[i];
+            negIndex += 2
+        }
     }
 
     return ans;
