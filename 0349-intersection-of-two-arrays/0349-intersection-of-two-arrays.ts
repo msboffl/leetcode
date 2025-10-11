8965b528bd89183a3e42d6da000ff2ca9216c252
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-11 22:37:58 +0530

    Time: 0 ms (100%), Space: 56.2 MB (76.29%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -1,10 +1,10 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    const ans: number[] = [];
+    const ans = [];
 
-    for (let i = 0; i < nums1.length; i++) {
-        for (let j = 0; j < nums2.length; j++) {
-            if (nums1[i] === nums2[j]) {
-                if (!ans.includes(nums1[i])) {  
+    for(let i = 0; i < nums1.length; i++) {
+        for(let j = 0; j < nums2.length; j++) {
+            if(nums1[i] === nums2[j]) {
+                if(!ans.includes(nums1[i])) {
                     ans.push(nums1[i]);
                 }
                 break;
@@ -13,4 +13,4 @@ function intersection(nums1: number[], nums2: number[]): number[] {
     }
 
     return ans;
-}
+};
