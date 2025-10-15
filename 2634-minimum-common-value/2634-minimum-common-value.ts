5c1b7e72a52cf23d86103f4846d50d6db9c949d2
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-15 12:50:19 +0530

    Time: 1 ms (92.92%), Space: 63.7 MB (65.49%) - LeetHub

---
+++ 2634-minimum-common-value/2634-minimum-common-value.ts
@@ -0,0 +1,11 @@
+function getCommon(nums1: number[], nums2: number[]): number {
+    let i = 0, j = 0;
+    
+    while(i < nums1.length && j < nums2.length) {
+        if(nums1[i] < nums2[j]) i++;
+        if(nums1[i] > nums2[j]) j++;
+        if(nums1[i] === nums2[j]) return nums1[i] || nums2[j];
+    }
+
+    return -1;
+};
