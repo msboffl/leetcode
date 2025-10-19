debaafd9ae7703232551613df9d487ffa0d0bde0
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-19 18:53:08 +0530

    Time: 0 ms (100%), Space: 59.2 MB (26.91%) - LeetHub

--- 4008-restore-finishing-order/4008-restore-finishing-order.ts
+++ 4008-restore-finishing-order/4008-restore-finishing-order.ts
@@ -1,11 +1,9 @@
 function recoverOrder(order: number[], friends: number[]): number[] {
-    const ans = [];
-    for(let i = 0; i < order.length; i++) {
-        for(let j = 0; j < friends.length; j++) {
-            if(order[i] === friends[j]) {
-                ans.push(order[i])
-            }
-        }
+    const ans = []
+
+    const friendSet = new Set(friends);
+    for(const id of order) {
+        if(friendSet.has(id)) ans.push(id)
     }
 
     return ans;
