af99dc4a0fafd64e7664907e3a7a109f510aea9c
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-22 16:08:00 +0530

    Time: 0 ms (100%), Space: 57.4 MB (71.65%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.ts
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.ts
@@ -1,11 +1,11 @@
 function differenceOfSums(n: number, m: number): number {
-    let num1 = 0;
-    let num2 = 0;
-
+    let NotDivisibleByM = 0;
+    let divisibleByM = 0;
+    
     for(let i = 1; i <= n; i++) {
-        if(i % m !== 0) num1 += i;
-        else num2 += i;
+        if(i % m !== 0) NotDivisibleByM += i;
+        else divisibleByM += i;
     }
 
-    return num1 - num2;
+    return NotDivisibleByM - divisibleByM;
 };
