8a53b8359e5ab0637af724c3f89cbb21b0314f98
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-23 19:53:43 +0530

    Time: 0 ms (100%), Space: 56.1 MB (6.67%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.ts
+++ 2556-convert-the-temperature/2556-convert-the-temperature.ts
@@ -1,5 +1,3 @@
 function convertTemperature(celsius: number): number[] {
-    const kelvin = celsius + 273.15;
-    const fahrenheit = celsius * 1.80 + 32.00
-    return [kelvin, fahrenheit];
+    return [celsius + 273.15, celsius * 1.80 + 32.00];
 };
