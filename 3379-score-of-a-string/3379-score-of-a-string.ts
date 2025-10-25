13ba72d49d98b0b5c60ae62b5352592a9c96a064
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-26 00:33:01 +0530

    Time: 1 ms (58.94%), Space: 58 MB (41.55%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.ts
@@ -0,0 +1,9 @@
+function scoreOfString(s: string): number {
+    let sum = 0;
+    for(let i = 0; i < s.length-1; i++) {
+        let num = Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1))
+        sum += num;
+    }
+
+    return sum;
+};
