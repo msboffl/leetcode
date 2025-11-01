f755170fe38a036bace5d1c8496a24ffcf12a49b
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-11-01 10:31:31 +0530

    Time: 65 ms (100%), Space: 106 MB (12.5%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function modifiedList(nums: number[], head: ListNode | null): ListNode | null {
+
+    let set = new Set(nums);
+
+    
+    while (head && set.has(head.val)) {
+        head = head.next;
+    }
+
+    let current: ListNode | null = head;
+
+    
+    while (current && current.next) {
+        if (set.has(current.next.val)) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+
+    return head;
+
+};
